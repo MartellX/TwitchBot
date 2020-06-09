3fdba588b3ec7900ba0a20d861ebea60004bda20
Martelly <kulbanikolai@yandex.ru>
2020-06-09 14:19:58 +0300

    Отключил анеки, небольшая обнова для арт

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,6 @@ public class Main {
         MainController.setGoogleSheets(googleCredsPath);
         MainController.setLogsFile("logs.txt");
         MainController.joinTo("martellx");
-        //MainController.joinTo("uselessmouth");
+        MainController.joinTo("uselessmouth");
     }
 }
